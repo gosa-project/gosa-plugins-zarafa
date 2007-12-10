914ff58de2c044ee1eb0fea79c537217947b150c
rettenbe <rettenbe@594d385d-05f5-0310-b6e9-bd551577e9d8>
2007-12-10 09:22:21 +0000

    update
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@8065 594d385d-05f5-0310-b6e9-bd551577e9d8

--- contrib/socket_server/client.php
+++ contrib/socket_server/client.php
@@ -5,11 +5,12 @@ require_once("../../include/class_socketClient.inc");
 error_reporting(E_ALL);
 
 $sock = new Socket_Client("10.89.1.155","9999",TRUE,1);
+#$sock = new Socket_Client("169.254.2.248","9999",TRUE,1);
 $sock->setEncryptionKey("ferdinand_frost");
 
 if($sock->connected()){
 	/* Prepare a hunge bunch of data to be send */
-	$data = "Hallo Andi. Alles Wird Toll.";
+	$data = "<xml><header>ping</header><source>192.168.1.36:10001</source><target>192.168.1.36:10000</target></xml>";
 	$sock->write($data);
   
   #$sock->setEncryptionKey("ferdinand_frost");
