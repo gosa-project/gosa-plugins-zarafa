f93a084d4b164c8392f288581d982fef55049bb2
cajus <cajus@594d385d-05f5-0310-b6e9-bd551577e9d8>
2008-02-26 17:05:06 +0000

    Fixed URL extraction
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@9134 594d385d-05f5-0310-b6e9-bd551577e9d8

--- gosa-si/modules/SIPackages.pm
+++ gosa-si/modules/SIPackages.pm
@@ -752,17 +752,17 @@ sub new_ldap_config {
 	@servers= sort (@servers);
 
 	foreach $server (@servers){
-		# Conversation for backward compatibility
-		if (not $server =~ /^ldap[^:]+:\/\// ) {
-		    if ($server =~ /^([^:]+):(.*)$/ ) {
-                $server= "1:dummy:ldap://$1/$2";
-		    }
-		}
+                # Conversation for backward compatibility
+                if (not $server =~ /^\d+:[^:]+:ldap[^:]*:\/\// ) {
+                    if ($server =~ /^([^:]+):(.*)$/ ) {
+                      $server= "1:dummy:ldap://$1/$2";
+                    }
+                }
 
-		$base= $server;
-		$server =~ s%^[^:]+:[^:]+:(ldap.*://[^/]+)/.*$%$1%;
-		$base =~ s%^[^:]+:[^:]+:ldap.*://[^/]+/(.*)$%$1%;
-		push (@ldap_uris, $server);
+                $base= $server;
+                $server =~ s%^[^:]+:[^:]+:(ldap.*://[^/]+)/.*$%$1%;
+                $base =~ s%^[^:]+:[^:]+:ldap.*://[^/]+/(.*)$%$1%;
+                push (@ldap_uris, $server);
 	}
 
 	# Assemble data package
