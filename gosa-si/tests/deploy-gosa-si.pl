0f42224c40c2f4a857046ee6967c46ae4d9e00d7
rettenbe <rettenbe@594d385d-05f5-0310-b6e9-bd551577e9d8>
2008-04-22 09:48:22 +0000

    small depoly script for gosa-si
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@10613 594d385d-05f5-0310-b6e9-bd551577e9d8

---
+++ gosa-si/tests/deploy-gosa-si.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  deploy-gosa-si.pl
+#
+#        USAGE:  ./deploy-gosa-si.pl 
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:   (), <>
+#      COMPANY:  
+#      VERSION:  1.0
+#      CREATED:  22.04.2008 11:28:43 CEST
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+my %copies = (
+        "gosa-si-server"=> "/usr/sbin/gosa-si-server", 
+        "modules/GosaPackages.pm" => "/usr/lib/gosa-si/modules/GosaPackages.pm",
+        "modules/SIPackages.pm" => "/usr/lib/gosa-si/modules/SIPackages.pm",
+        "modules/DBsqlite.pm" => "/usr/share/perl5/GOSA/DBsqlite.pm",
+        "modules/GosaSupportDaemon.pm" => "/usr/share/perl5/GOSA/GosaSupportDaemon.pm",
+        "sever/events/clMessages.pm" => "/usr/lib/gosa-si/server/events/clMessages.pm",
+        "sever/events/databases.pm" => "/usr/lib/gosa-si/server/events/databases.pm",
+        "sever/events/gosaTriggered.pm" => "/usr/lib/gosa-si/server/events/gosaTriggered.pm",
+        "sever/events/siTriggered.pm" => "/usr/lib/gosa-si/server/events/siTriggered.pm",
+);
+
+while( my($file_name, $new_file) = each %copies ) {
+    print STDERR "copy ../$file_name to $new_file\n"; 
+    system("cp ../$file_name $new_file"); 
+}
+
+
