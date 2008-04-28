21bb1916883809cccf6caa8fd5b328d447963116
rettenbe <rettenbe@594d385d-05f5-0310-b6e9-bd551577e9d8>
2008-04-28 13:23:57 +0000

    enhancement: more self speaking daemon_log at new_ldap_config
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@10713 594d385d-05f5-0310-b6e9-bd551577e9d8

--- gosa-si/modules/SIPackages.pm
+++ gosa-si/modules/SIPackages.pm
@@ -677,7 +677,7 @@ sub new_ldap_config {
 	# check hit
 	my $hit_counter = keys %{$res};
 	if( not $hit_counter == 1 ) {
-		&main::daemon_log("$session_id ERROR: more or no hit found in known_clients_db by query by '$address'", 1);
+		&main::daemon_log("$session_id ERROR: more or no hit found in known_clients_db by query '$sql_statement'", 1);
 	}
 
     $address = $res->{1}->{hostname};
