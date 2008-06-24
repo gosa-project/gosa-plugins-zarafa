aef400791f4731df5594ed456471323396d84437
cajus <cajus@594d385d-05f5-0310-b6e9-bd551577e9d8>
2008-06-24 15:26:56 +0000

    Added missing functions
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@11426 594d385d-05f5-0310-b6e9-bd551577e9d8

--- gosa-si/server/events/opsi.pm
+++ gosa-si/server/events/opsi.pm
@@ -6,11 +6,14 @@ my @events = (
     "opsi_get_netboot_products",
     "opsi_get_local_products",
     "opsi_get_product_properties",
-    "opsi_set_product_properties",
+    "opsi_get_client_hardware",
+    "opsi_get_client_software",
     "answer_opsi_get_netboot_products",
     "answer_opsi_get_local_products",
     "answer_opsi_get_product_properties",
     "answer_opsi_set_product_properties",
+    "answer_opsi_get_client_hardware",
+    "answer_opsi_get_client_software",
    );
 @EXPORT = @events;
 
@@ -57,6 +60,17 @@ sub opsi_get_local_products {
         return ( $msg );
 }
 
+sub opsi_get_client_hardware {
+        my ($msg, $msg_hash, $session_id) = @_;
+        $msg =~ s/gosa_opsi/opsi/g;
+        return ( $msg );
+}
+
+sub opsi_get_client_software {
+        my ($msg, $msg_hash, $session_id) = @_;
+        $msg =~ s/gosa_opsi/opsi/g;
+        return ( $msg );
+}
 
 sub answer_opsi_get_netboot_products {
     my ($msg, $msg_hash, $session_id) = @_ ;
@@ -77,6 +91,18 @@ sub answer_opsi_set_product_properties {
 }
 
 sub answer_opsi_get_local_products {
+    my ($msg, $msg_hash, $session_id) = @_ ;
+    $msg =~ s/<target>\S+<\/target>/<target>GOSA<\/target>/g;
+    return ($msg);
+}
+
+sub answer_opsi_get_client_hardware {
+    my ($msg, $msg_hash, $session_id) = @_ ;
+    $msg =~ s/<target>\S+<\/target>/<target>GOSA<\/target>/g;
+    return ($msg);
+}
+
+sub answer_opsi_get_client_software {
     my ($msg, $msg_hash, $session_id) = @_ ;
     $msg =~ s/<target>\S+<\/target>/<target>GOSA<\/target>/g;
     return ($msg);
