5473ceb4615701376d123702427465e8c3ed3522
rettenbe <rettenbe@594d385d-05f5-0310-b6e9-bd551577e9d8>
2008-11-06 17:04:41 +0000

    new opsi_client to debug ospi problems
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@12947 594d385d-05f5-0310-b6e9-bd551577e9d8

--- gosa-si/server/events/opsi_com.pm
+++ gosa-si/server/events/opsi_com.pm
@@ -429,7 +429,8 @@ sub opsi_get_netboot_products {
 
     &main::daemon_log("$session_id DEBUG: send callobj to opsi_client: ".&opsi_callobj2string($callobj), 7);
     &main::daemon_log("$session_id DEBUG: waiting for answer from opsi_client!", 7);
-    my $res = $main::opsi_client->call($main::opsi_url, $callobj);
+    my $opsi_client =  new JSON::RPC::Client;
+    my $res = $opsi_client->call($main::opsi_url, $callobj);
     &main::daemon_log("$session_id DEBUG: get answer from opsi_client with number of entries: ".length(@$res), 7);
     my %r = ();
     for (@{$res->result}) { $r{$_} = 1 }
