285ded67852fef8aa2aeec6f16d36bc593f121d5
rettenbe <rettenbe@594d385d-05f5-0310-b6e9-bd551577e9d8>
2009-03-06 15:13:21 +0000

    changing file permissions of /var/log/gosa-si-client.log, /etc/ldap/ldap-shell.conf, /var/run/gosa-si-client.opts, /var/log/fai/..., /var/lib/gosa-si/*.db*
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@13507 594d385d-05f5-0310-b6e9-bd551577e9d8

--- gosa-si/server/events/clMessages.pm
+++ gosa-si/server/events/clMessages.pm
@@ -101,9 +101,11 @@ sub save_fai_log {
         my ($log_file, $log_string) = split(":", $log);
         my $client_fai_log_file = File::Spec->catfile( $client_fai_log_dir, $log_file);
 
-	open(my $LOG_FILE, ">$client_fai_log_file"); 
-        print $LOG_FILE &decode_base64($log_string);
-        close($LOG_FILE);
+		open(my $LOG_FILE, ">$client_fai_log_file"); 
+		print $LOG_FILE &decode_base64($log_string);
+		close($LOG_FILE);
+		chown($main::root_uid, $main::adm_gid, $client_fai_log_file);
+		chmod(0440, $client_fai_log_file);
 
     }
     return;
